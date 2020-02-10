acb6440c27ae81b88e57472a3844f3f93218bea3
Federico Bit <fedechiappero06@gmail.com>
2020-02-09 22:10:09 -0300

    Add requerimientos del software

--- main.bat
+++ main.bat
@@ -1,2 +1,5 @@
 @echo off
 echo Este "programa" servira para mostrar funcionalidad basica de git
+echo ----------------------------------------------------------------
+echo Requerimientos: dividir dos numeros
+echo var1 / var2 = ??
